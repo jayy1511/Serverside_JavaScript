b8e830f41d4c1232acc16f963b3121bfe84dfcd3
jayy1511 <jay1511dj@gmail.com>
2025-03-17 10:11:46 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const userRoutes = require("./routes/users")
+
+app.use("/api/users", userRoutes)
 
 app.use((req, res, next) => {
   req.calculatedValue = 4 * 7;
@@ -11,6 +14,10 @@ app.get('/', (req, res) => {
   console.log(`Calculated value: ${req.calculatedValue}`);
   res.send(`The calculated value is: ${req.calculatedValue}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my API ! e-commerce backed 🤳")
+ })
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
---
+++ src/routes/users.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.send('Users page')
+})
+
+router.post('/', (req, res) => {
+    res.send('you have reached the POST request !')
+})
+
+module.exports = router
