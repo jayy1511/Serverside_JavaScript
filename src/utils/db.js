5aab529af9265f887f490c3e85782a6a36e86493
jayy1511 <jay1511dj@gmail.com>
2025-03-21 16:58:47 +0100

    db connection added

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-const mongoConnection = process.env.MONGO_URI; // Load from .env
+const mongoConnection = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/e_commerce_db"; // fallback for local
 
 mongoose.set("strictQuery", true);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(mongoConnection);
+        await mongoose.connect(mongoConnection, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
         console.log("✅ Connected to MongoDB");
     } catch (error) {
         console.error("❌ No DB connection!", error);
