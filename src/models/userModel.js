726c87416d328861ee16ada967584f63a989c89d
jayy1511 <jay1511dj@gmail.com>
2025-04-21 22:54:14 +0200

    timestamp added and user model created

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -16,7 +16,7 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true, // No duplicate accounts
+    unique: true,
   },
   password: {
     type: String,
